a0cbcf3642ecf65cddbc9a0b433afc248cff21ab
martinavagyan <martinavagyan@gmail.com>
2016-12-07 15:41:57 +0100

    service change 2

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template,request
+import requests
+
 app = Flask(__name__)
+
 
 from flask import render_template
 
 @app.route('/')
 def index():
     return render_template('index.html')
+	
+@app.route('/get-ex')
+def test():
+    return app.send_static_file('simple.html')
 
-
+	
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
