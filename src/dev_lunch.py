0bb5f242a542cb0ab342a2ed497aaa46e918c990
martinavagyan <martinavagyan@gmail.com>
2017-05-25 16:04:40 +0200

    fixing heroku

--- src/dev_lunch.py
+++ src/dev_lunch.py
@@ -1,10 +1,3 @@
 import os
 from zeeguu_exercises.app import app
-
-
-if 'PORT' in os.environ:
-  port = int(os.environ['PORT'])
-else:
-  port = 5000
-
-app.run(host='0.0.0.0', debug=True, port=port);
+app.run(host='0.0.0.0', debug=True);
