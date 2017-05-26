2357811d54d32e56b54fd2c66e320f1e1d540902
martinavagyan <martinavagyan@gmail.com>
2017-05-26 10:56:58 +0200

    shake animation added

--- src/zeeguu_exercises/static/js/app/shake_animation.js
+++ src/zeeguu_exercises/static/js/app/shake_animation.js
@@ -1,15 +1,57 @@
 /**
- * Created by Martin on 5/26/2017.
- */
-/**
- * TODO add documentation
- * example usage <div class="shakable"></div>
+ * This class is responsible for animating elements that have a class attribute "shakable".
+ * When an element is clicked with class "shakable", class "shake" is added to its attributes.
+ * After the animation is done the "shake" class is removed.
+ * In HTML use the class "shakable" for each element that shake property
+ * @Example <div class="shakable"></div>
  * */
 
-var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
-$('.shakable').click(function () {
-  $(this).addClass('shake wrongAlert');
-  $(this).one(animationEvent, function(event) {
-    $(this).removeClass('shake')
-  });
-});
+import $ from 'jquery';
+
+export default class ShakeAnimation{
+
+    constructor(){
+        this.bindUIActions();
+    }
+
+    /**
+     * Make all the actions connected to this module
+     * return {void}
+     * */
+    bindUIActions(){
+        var _this = this;
+        $('.shakable').on("click", _this.shake.bind(this));
+    }
+
+    /**
+     * Use the clicked element to invoke shakeElement
+     * @return {void}
+     * */
+    shake(){
+        this.shakeElement($(this));
+    }
+
+    /**
+     * Function adds class shake, after the animation is done,
+     * the class shake is removed from the element
+     * @param {jQuery element}, elem
+     * @return {void}
+     * */
+    shakeElement(elem){
+        var animationEvent = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
+        elem.addClass('shake wrongAlert');
+        elem.one(animationEvent, function(event) {
+            elem.removeClass('shake')
+        });
+    }
+
+    /**
+     * Make the given element shakable
+     * @param {Object}, element
+     * @return {void}
+    * */
+    makeShakable(elem){
+        elem.addClass('shakable');
+        this.bindUIActions();
+    }
+}
