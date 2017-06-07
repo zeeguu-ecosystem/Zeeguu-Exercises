b46d4901c654e7bcb763c2f9641b9a662ec70d5b
martinavagyan <martinavagyan@gmail.com>
2017-06-07 21:52:22 +0200

    redirect from test

--- src/zeeguu_exercises/static/js/app/debug/set_cookie_test.js
+++ src/zeeguu_exercises/static/js/app/debug/set_cookie_test.js
@@ -1,3 +1,4 @@
 import Session from '../session';
 Session.setSession();
-window.location.replace(document.location.hostname);
+let redirect = window.location.protocol + "//" + window.location.hostname;
+window.location.href = (redirect === "http://127.0.0.1")? redirect+":5000":redirect;
