c9825dde0f5904302131c8f5d76c32f7ef4e92ea
martinavagyan <martinavagyan@gmail.com>
2017-06-23 00:21:29 +0200

    fix the typo

--- src/zeeguu_exercises/static/js/app/speaker.js
+++ src/zeeguu_exercises/static/js/app/speaker.js
@@ -4,23 +4,13 @@
 export default class Speaker {
 
     /**
-     * @param {String} txt - the text to be read.
-     * @param {String} lang - red the text in language. @example 'nl-NL'
-
+     * @param {String} txt - the text to be spoken.
+     * @param {String} lang - speak the text in language. @example 'nl'
      */
     static speak(txt, lang) {
-        lang  = Speaker.formatLanguage(lang);
         let spkr = new SpeechSynthesisUtterance();
         spkr.text = txt;
         spkr.lang = lang;
         speechSynthesis.speak(spkr);
-    }
-
-    /**
-     * Format the language from zeeguu API to SpeechSynthesisUtterance API
-     * For now given the 4 languages this solution works, later it will need modification
-     * */
-    static formatLanguage(lang) {
-        return lang + "-" + lang.toUpperCase();
     }
 };
